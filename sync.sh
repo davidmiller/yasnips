12de0738e91fbf45a80500b0efdb8e96a85c1fec
David <david@deadpansincerity.com>
2010-11-03 14:26:43 +0000

    Auto Commit from Epicurus

---
+++ sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rsync -arvuz /home/david/.emacs.d/yasnippet/snippets/text-mode/python-mode/ python-mode/
+rsync -arvuz /home/david/.emacs.d/yasnippet/snippets/text-mode/django-mode/ django-mode/
+rsync -arvuz /home/david/.emacs.d/yasnippet/snippets/text-mode/html-mode/ html-mode/
+rsync -arvuz /home/david/.emacs.d/yasnippet/snippets/text-mode/javascript-mode/ javascript-mode
+/rsync -arvuz /home/david/.emacs.d/yasnippet/snippets/text-mode/nxml-mode/ nxml-mode/
+rsync -arvuz /home/david/.emacs.d/yasnippet/snippets/text-mode/php-mode/ php-mode/
+git add .
+git commit -a -m "Auto Commit from Epicurus"
+git push origin master
